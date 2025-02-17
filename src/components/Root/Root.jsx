57f86a436b818541eb707f119eca785c5202da99
hellosiyam <hellosiyambd@gmail.com>
2025-02-17 23:39:45 +0600

    mi-8-mo-47-Book-vibe

---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,10 @@
+
+const Root = () => {
+    return (
+        <div>
+            <h1>Hello Form Root</h1>
+        </div>
+    );
+};
+
+export default Root;
--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,12 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Root from './components/Root/Root.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div className='text-center'>Hello world!</div>,
+    element: <Root></Root>,
   },
 ]);
 
