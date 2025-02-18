5c918f70d830c15f56b5406dd61ad8ba86659053
hellosiyam <hellosiyambd@gmail.com>
2025-02-19 01:11:58 +0600

    mi-8-mo-47-Book-Detail-Add-read-list

--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -12,7 +12,7 @@ const Books = () => {
 
     return (
         <div>
-            <h2 className="title text-5xl text-black font-bold text-center">Books</h2>
+            <h2 className="my-5 lg:my-10 title text-5xl text-black font-bold text-center">Books</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 {
                     books.map(book => <Book key={book.bookId} book={book}></Book>)
