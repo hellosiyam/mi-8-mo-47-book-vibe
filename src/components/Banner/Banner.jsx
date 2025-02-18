b65ecabd6367c5fb2707418415826dfe5f018ab6
hellosiyam <hellosiyambd@gmail.com>
2025-02-18 15:08:22 +0600

    mi-8-mo-47-Book-section

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -8,7 +8,7 @@ const Banner = () => {
                     className='' />
                 <div>
                     <h1 className="text-4xl lg:text-6xl font-bold mb-5 lg:mb-10 title ">Books to freshen up  your bookshelf</h1>
-                    <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg text-xl text-white font-bold px-10 py-7 bg-[#23BE0A] rounded-xl">View The List</button>
+                    <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg text-xl text-white font-bold px-10 py-7 bg-[#23BE0A] hover:bg-green-700 rounded-xl">View The List</button>
                 </div>
             </div>
     );
