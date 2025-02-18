c10ea4c34a26be17b434e1bf69f79ccc3c9801f9
hellosiyam <hellosiyambd@gmail.com>
2025-02-18 12:14:00 +0600

    mi-8-mo-47-Book-vibe

---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,10 @@
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h1>This is the Dash Dashboard</h1>
+        </div>
+    );
+};
+
+export default Dashboard;
--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,31 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import Root from './components/Root/Root.jsx';
+import Error from './components/Error/Error.jsx';
+import Home from './components/Home/Home.jsx';
+import Dashboard from './components/Dashboard/Dashboard.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement : <Error></Error>,
+    children : [
+      {
+        path : '/',
+        element : <Home></Home>
+      },
+      {
+        path : '/dashboard',
+        element : <Dashboard></Dashboard>
+      }
+    ]
   },
 ]);
 
